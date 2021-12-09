9786b6cae47be8a0db69b34591cabaf94cdc1908
shopifydev-gk <shopifydevgk@gmail.com>
2021-12-09 22:26:22 +0200

    Saved products to products array after ajax call

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -3,49 +3,54 @@ import { Flex, Box, Text } from '@chakra-ui/layout';
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
-import VariantOptions from '../components/VariantOptions';
 import { ShopContext } from '../contexts/ShopContextProvider';
 
 function ProductPage() {
   const { handle } = useParams();
 
+  const [currentProduct, setCurrentProduct] = useState();
+
   const [selectedVariant, setSelectedVariant] = useState();
 
-  const { product, fetchProductByHandle } = useContext(ShopContext);
+  const { product, products, updateProducts, fetchProductByHandle } = useContext(ShopContext);
 
   useEffect(() => {
-    fetchProductByHandle(handle).then(() => {
-      if (product.handle) {
-        setSelectedVariant(product.variants.find((variant) => variant.available));
-      }
-    });
-  }, [fetchProductByHandle, handle, product.handle]);
+    const hasMatchingProduct = products.find((x) => x.handle === handle);
 
-  const renderVariantOptions = () => {
-    return <VariantOptions activeVariant={selectedVariant} options={product.options} />;
-  };
+    if (hasMatchingProduct) {
+      setCurrentProduct(hasMatchingProduct);
+      setSelectedVariant(hasMatchingProduct.variants.find((variant) => variant.available));
+    } else {
+      fetchProductByHandle(handle).then(() => {
+        if (product.handle) {
+          setCurrentProduct(product);
+          setSelectedVariant(product.variants.find((variant) => variant.available));
+          updateProducts(product);
+        }
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [product.handle]);
 
-  if (!product.handle) return <p>...Loading</p>;
+  if (!currentProduct) return <p>...Loading</p>;
 
   return (
     <div className='product-page'>
       <div className='breadcrumbs'>
-        <Link to='/'>Homepage</Link>/<span>{product.title}</span>
+        <Link to='/'>Homepage</Link>/<span>{currentProduct.title}</span>
       </div>
 
       <Flex>
         <Box w='50%'>
-          <Image objectFit='cover' src={product.variants[0].image.src} />
+          <Image objectFit='cover' src={currentProduct.variants[0].image.src} />
         </Box>
 
         <Box w='50%'>
           <Text mb='1rem' fontWeight='bold' fontSize='2.4rem'>
-            {product.title}
+            {currentProduct.title}
           </Text>
 
-          {product.description && <Text>{product.description}</Text>}
-
-          {product.variants.length > 1 && renderVariantOptions()}
+          {currentProduct.description && <Text>{currentProduct.description}</Text>}
         </Box>
       </Flex>
     </div>
